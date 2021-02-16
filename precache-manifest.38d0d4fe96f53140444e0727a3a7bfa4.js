f38167f04e0596fcbcecd5cf18baf68917f6d96f
lsh <sermyes@gmail.com>
2021-02-16 23:57:19 +0900

    Updates

---
+++ precache-manifest.38d0d4fe96f53140444e0727a3a7bfa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fddea298d3fe861ff0c100b4e127372",
+    "url": "/businessMaker/index.html"
+  },
+  {
+    "revision": "7dd4ac26eeade7af3c5d",
+    "url": "/businessMaker/static/css/main.a3eb300a.chunk.css"
+  },
+  {
+    "revision": "805476a2def6127d6b3d",
+    "url": "/businessMaker/static/js/2.4a227e4b.chunk.js"
+  },
+  {
+    "revision": "c6d60d51e6f0d6fcb0bc107576265c92",
+    "url": "/businessMaker/static/js/2.4a227e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd4ac26eeade7af3c5d",
+    "url": "/businessMaker/static/js/main.403797a1.chunk.js"
+  },
+  {
+    "revision": "de87c0327516fadfb3f3",
+    "url": "/businessMaker/static/js/runtime-main.96cfa354.js"
+  }
+]);
