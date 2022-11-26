44d752b957c0cdb84dfcac3c8ccb2fe9173cbd30
lsh <sermyes@gmail.com>
2022-11-26 18:57:10 +0900

    Updates

---
+++ precache-manifest.46ea10ecd8777a56020c67b5b0ae158f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40b7dfca2b64e18e2b33f8e1f65b0e9",
+    "url": "/businessMaker/index.html"
+  },
+  {
+    "revision": "4f479cf6d7c39053e084",
+    "url": "/businessMaker/static/css/main.ae5f7092.chunk.css"
+  },
+  {
+    "revision": "9cbcca606b46ad3960bd",
+    "url": "/businessMaker/static/js/2.d7137f12.chunk.js"
+  },
+  {
+    "revision": "007c0435f20031ad0c7fb4cd4c120172",
+    "url": "/businessMaker/static/js/2.d7137f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f479cf6d7c39053e084",
+    "url": "/businessMaker/static/js/main.5cb38782.chunk.js"
+  },
+  {
+    "revision": "c94eb9ece6313f58c9eb",
+    "url": "/businessMaker/static/js/runtime-main.c71387fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/businessMaker/precache-manifest.0ae13b95f8e58a145ff9a2d235e9234c.js"
+  "/businessMaker/precache-manifest.46ea10ecd8777a56020c67b5b0ae158f.js"
 );
 
 self.addEventListener('message', (event) => {
